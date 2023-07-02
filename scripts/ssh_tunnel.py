5b84342958d301859f3f63749c8e60dbc9168ec3
phamhungd <phamhung.d@gmail.com>
2023-07-03 03:05:05 +0700

    update

--- scripts/ssh_tunnel.py
+++ scripts/ssh_tunnel.py
@@ -147,5 +147,5 @@ if cmd_opts.multiple:
         os.environ['GRADIO_TUNNEL'] = gradio_tunnel()
     except:
         pass
-    strings.en["RUNNING_LOCALLY_SEPARATED"] = f"Public WebUI Colab URL: {os.getenv('REMOTE_MOE')} \nPublic WebUI Colab URL: {os.getenv('GRADIO_TUNNEL')} \nPublic WebUI Colab URL: {os.getenv('LOCALHOST_RUN')}"
-    strings.en["SHARE_LINK_DISPLAY"] = "Please do not use this link we are getting ERROR: Exception in ASGI application:  {}"
+    strings.en["RUNNING_LOCALLY_SEPARATED"] = f"Link Sever Remote: {os.getenv('REMOTE_MOE')} \nBankup Link 1: {os.getenv('GRADIO_TUNNEL')} \nBankup Link 2: {os.getenv('LOCALHOST_RUN')}"
+    strings.en["SHARE_LINK_DISPLAY"] = "Link Sever Gradio:  {}"
